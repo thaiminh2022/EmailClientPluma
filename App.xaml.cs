10224dcae9d04304e5f6598e1b9def00ecdad29b
Thaiminh2022 <thaiminh2022@gmail.com>
2025-09-24 09:28:18 +0700

    Basic DI and minimal UI

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using EmailClientPluma.Core.Services;
+using EmailClientPluma.MVVM.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 namespace EmailClientPluma
@@ -9,6 +10,29 @@ namespace EmailClientPluma
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider Services { get; private set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = new MainView()
+            {
+                DataContext = Services.GetRequiredService<MainViewModel>()
+            };
+            mainWindow.Show();
+        }
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IAuthenticationService, AuthenticationService>();
+            services.AddSingleton<IStorageService, StorageService>();
+
+            // Might change this later, it's singleton due to aplication design
+            services.AddSingleton<MainViewModel>();
+
+            Services = services.BuildServiceProvider();
+        }
     }
 
 }
---
+++ Core/Services/StorageService.cs
@@ -0,0 +1,11 @@
+﻿namespace EmailClientPluma.Core.Services
+{
+    interface IStorageService
+    {
+
+    }
+    internal class StorageService : IStorageService
+    {
+
+    }
+}
