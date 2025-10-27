b9ea247d16bd8c48ddbac296ea5d021b23ea734a
Thaiminh2022 <thaiminh2022@gmail.com>
2025-10-27 10:23:41 +0700

    Revert "Update PhishDetector feature"
    This reverts commit b5aac3948e3aafaa99b984ae38d12f42e1e5fb5e.

--- Behaviors/WebView2Html.cs
+++ Behaviors/WebView2Html.cs
@@ -26,32 +26,7 @@ namespace EmailClientPluma.Behaviors
             // if html is invalid, tell them it's in valid
             if (!html.Contains("<base", System.StringComparison.OrdinalIgnoreCase))
                 html = html.Replace("<head>", "<head><base href=\"https://app.invalid/\">");
-            //Check phising before view email
-            var links = PhishDetector.ExtractUrlsFromHtml(html);
-            var trustedDomains = new[] { "gmail.com", "outlook.com", "apple.com", "bank.com" };
-            int score = PhishDetector.ScoreLinks(links, trustedDomains);
 
-            if (score >= 80)
-            {
-                MessageBox.Show(
-                    "WARNING: This email shows signs of phishing!\nBe careful when clicking on links.",
-                    "Security alert",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Warning
-                );
-            }
-            else if (score >= 50)
-            {
-                MessageBox.Show(
-                    "This email has some suspicious signs.",
-                    "Low warning",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Information
-                );
-            }
-            // ----------------------------------------------------
-
-            // Continue viewing email
             wv2.CoreWebView2.NavigateToString(html);
         }
 
